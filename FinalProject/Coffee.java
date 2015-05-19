48d8c7ddafa5995feaeecf747ec6ba7d82ef3a8b
Rachel <rachel.meltser@gmail.com>
2015-05-19 09:22:03 -0500

    buttons

--- FinalProject/Coffee.java
+++ FinalProject/Coffee.java
@@ -11,12 +11,21 @@ import java.applet.*;
  * @Rachel Meltser
  * @5/14/2015
  */
-public class Coffee extends JApplet implements MouseListener
+public class Coffee extends JApplet implements MouseListener, ActionListener
 {
     private Image coffeeCup;
-    private int score;
+    private int score, multiplier=1;
+    public Button b1,b2,b3;
     public void init(){
+        //instantiates coffee cup
         coffeeCup=getImage(getDocumentBase(), "cup-of-black-coffee1.jpg");
+        //creates buttons
+        Button b1 = new Button("2X Multiplier");
+        Button b2 = new Button("3X Multiplier");
+        Button b3 = new Button("5X Multiplier");
+        this.add(b1);//makes the entire applet a button -->need to change its size
+        b1.addActionListener(this);//makes it button-y
+        
     }
     public void paint(Graphics g)
     {
@@ -24,12 +33,13 @@ public class Coffee extends JApplet implements MouseListener
         int x = 0;
         int y = 0;
         int width = 500;
-        int height = 450;        
+        int height = 450;
+        //draws coffee cup
         g.drawImage(coffeeCup, x, y, width, height, this);
-        g.drawString("Score: "+score,25,25);
+        g.drawString("Score: "+score,25,25);        
     }
     public void mouseClicked(MouseEvent me){
-        score++;
+        score+=multiplier;        
         repaint();
     } 
     public void mouseEntered(MouseEvent me){
@@ -44,4 +54,12 @@ public class Coffee extends JApplet implements MouseListener
     public void mouseExited(MouseEvent me){
         
     } 
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==b1){
+            if(score>=100){
+                score-=100;
+                multiplier=2;
+            }
+        }
+    }
 }
